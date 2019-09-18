7a0f162e6b1067faf1ff2e1d9c0e2eeaa96522bd
EvelynMarleth <emontoy@ucol.mx>
2019-09-17 19:35:38 -0500

    consola

---
+++ 03-conceptos-basicos/main.js
@@ -0,0 +1,50 @@
+/*Universiddad de colima
+Facultad de Telematica
+Fundamentos de programacion
+*/
+var nombre ='Juan Perez'
+var edad = 22
+var esEstudiante =true
+var email
+var numero1 = 5
+var numero2 = 2
+
+console.log
+(
+    nombre +' tiene '+ edad + ' años. ¿Es estudiante? ' + esEstudiante
+)
+
+console.log(50 + 10 / 8 +2)
+console.log((50 + 10) / (8 +2))
+
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log(numero1 % numero2)
+
+var resultado = numero1 + numero2
+console.log(resultado)
+
+
+
+
+var numero
+var numeroCuenta
+var numeroCuentaEmpleado
+//Ejemplo de conceptos Basicos
+
+console.log('Hola, mundo!')
+console.log(nombre)
+console.log(edad)
+console.log(email)
+
+edad = 'veintidos'
+console.log(edad)
+
+
+nombre = prompt ('como te llamas?')
+alert ('Hola '+nombre)
+
+
+
